6021f91b7dae4dca8ce1742a52c8c2159d82a273
do1lmoon <dismoon86@gmail.com>
2017-12-26 10:46:50 +0900

    news_proto

---
+++ viewpage/src/js/list.js
@@ -0,0 +1,9 @@
+function openList1() {
+    var list = document.getElementById("ollist");
+
+    if (list.style.display == "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none";
+    }
+}
